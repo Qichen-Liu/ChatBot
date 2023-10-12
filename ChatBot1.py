623af0ad373bd6a89d3a937c37ccb3651e3de952
Qichen-Liu <ql2326@nyu.edu>
2023-10-12 00:42:18 -0400

    added three bot

---
+++ ChatBot1.py
@@ -0,0 +1,25 @@
+# chatgpt inside a infinite loop
+import openai
+
+botKey = ""
+openai.api_key = botKey
+
+messages = []
+print("what type of chatbot would you like to create?")
+system_msg = input()
+messages.append({"role":"user", "content":"system_msg"})
+
+print("Your new assistant is ready")
+
+while input != "quit":
+    message = input()
+    if message == "quit":
+        break
+    messages.append({"role":"user", "content":message})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=messages
+    )
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role":"assistant", "content":reply})
+    print("\n" + reply + "\n")
---
+++ ChatBot2.py
@@ -0,0 +1,25 @@
+# chatgpt with gradio
+import openai
+import gradio
+
+botKey = ""
+openai.api_key = botKey
+
+# config the bot
+messages = [{"role": "system", "content": "You are a software engineering tech recruiter"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    # call ChatGPT to get response
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    #messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+# use gradio to create a simple UI
+demo = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="My Interview Trainer")
+
+demo.launch(share=True)
